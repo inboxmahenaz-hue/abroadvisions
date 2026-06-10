ce0b407a414fd65dd76d2ee0d2e295b12a78ff31
abroadvisions <inboxmahenaz@gmail.comapp/layout.tsx>
2026-06-11 00:42:16 +0530

    Add Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Geist } from 'next/font/google'
 import { Fraunces } from 'next/font/google'
 import './globals.css'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -65,6 +66,7 @@ export default function RootLayout({
     >
       <body className="font-sans antialiased">
         {children}
+        <GoogleTagManager gtmId="GTM-N3RGS23Z" />
       </body>
     </html>
   )
